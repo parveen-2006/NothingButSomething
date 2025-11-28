a00a3d135349cc461aad7f63bc3aee97290e9a37
parveen-2006 <pavanbaghel2006@gmail.com>
2025-11-28 10:36:25 +0530

    Add files via upload

---
+++ myapp/src/App.jsx
@@ -0,0 +1,60 @@
+import { createContext, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Home from './componant/Home'
+import SignIN from './componant/SignIN'
+import SignUp from './componant/SignUp'
+import Nav from './componant/Nav'
+import Profile from './componant/Profile'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import View from './componant/View'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/signin",
+        element: <SignIN />
+      },
+      {
+        path: "/signup",
+        element: <SignUp />
+      },
+      {
+        path:"/profile",
+        element: <Profile/>
+      },
+      {
+        path:"/view",
+        element: <View/>
+      }
+    ]
+  }
+])
+let userContext = createContext();
+
+function App() {
+
+  let [userdata, setUserdata] = useState("")
+
+  return (
+    <>
+      <userContext.Provider value={[userdata, setUserdata]}>
+        <RouterProvider router={router} />
+      </userContext.Provider>
+
+    </>
+  )
+}
+
+export default App
+export {userContext}
